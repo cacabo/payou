f7351864fa863eca7e5ed34d81d4c7980824fd75
ccabo1 <ccabo@seas.upenn.edu>
2018-10-31 16:20:53 -0400

    First commit

---
+++ components/Grid.js
@@ -0,0 +1,6 @@
+import s from 'styled-components'
+
+export const Container = s.div`
+  width: 100%;
+  padding: 0 5%;
+`
---
+++ pages/b.js
@@ -0,0 +1,10 @@
+import Layout from '../fragments/Layout/index'
+import { Container } from '../components'
+
+export default () => (
+  <Layout>
+    <Container>
+      <p>b</p>
+    </Container>
+  </Layout>
+)
