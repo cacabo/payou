ec761d83c90b6afdb0dce6f1f0d09f10b2a570ee
ccabo1 <ccabo@seas.upenn.edu>
2019-03-25 01:10:13 -0400

    Adding icons

--- fragments/Home/Purpose.js
+++ fragments/Home/Purpose.js
@@ -10,10 +10,18 @@ import {
   Row,
   Col,
 } from '../../components'
-import { BORDER } from '../../constants/colors'
+import { BORDER, SKY } from '../../constants/colors'
 
 const StyledRow = s(Row)`
   border: 1px solid ${BORDER};
+`
+
+const Icon = s.img`
+  height: 4rem;
+  background: ${SKY};
+  padding: 0.5rem;
+  border-radius: 8px;
+  margin-bottom: 0.5rem;
 `
 
 const StyledCol = s(Col)`
@@ -21,6 +29,15 @@ const StyledCol = s(Col)`
   padding: 1rem;
   border: 1px solid ${BORDER};
 `
+
+const icons = [
+  'honeymoon.svg',
+  'ring.svg',
+  'umbrella.svg',
+  'house.svg',
+  'money.svg',
+  'bill.svg',
+]
 
 const content = [
   'Take that honeymoon trip you’ve always dreamed of',
@@ -38,8 +55,9 @@ export default () => (
       <Title>Make the most of the good moments in life</Title>
 
       <StyledRow>
-        {content.map(c => (
+        {content.map((c, idx) => (
           <StyledCol sm={12} md={6} lg={4} key={c}>
+            <Icon src={`/static/img/icons/${icons[idx]}`} alt={icons[idx]} />
             <Text marginBottom="0">{c}</Text>
           </StyledCol>
         ))}
