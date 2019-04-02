d1fe756f0174ae34de5f835cb360fe0a4ac1a459
ccabo1 <ccabo@seas.upenn.edu>
2019-04-02 17:29:09 -0400

    working on employee schema

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,87 @@
+// const bcrypt = require('bcrypt')
+
 require('./mongooseConnect')
 
 const EmployerLead = require('./models/EmployerLead')
 const EmployeeLead = require('./models/EmployeeLead')
+const Employee = require('./models/Employee')
+
+
+function getEmployee(id) {
+  if (!id) throw new Error('Employee ID required')
+  const e = Employee.findById(id)
+  if (!e) throw new Error('Employee not found')
+}
+
+
+function execEmployeeAppStep1({
+  loanAmount,
+  loanTerm,
+}) {
+  return new Employee({
+    loanAmount,
+    loanTerm,
+    step: 2,
+  })
+}
+
+
+function execEmployeeAppStep2({
+  _id,
+  firstName,
+  lastName,
+  suffix,
+  dateOfBirth,
+  phone,
+}) {
+  const e = getEmployee(_id)
+
+  const { day, month, year } = dateOfBirth
+  if (!day || !month || !year) {
+    throw new Error('Day, month, and year are required for date of birth')
+  }
+
+  e.firstName = firstName
+  e.lastName = lastName
+  e.suffix = suffix
+  e.dateOfBirth = { day, month, year }
+  e.phone = phone
+  e.step = 3
+
+  return e.save()
+}
+
+
+function execEmployeeAppStep3({
+  _id,
+  email,
+  confirmEmail,
+  password,
+  confirmPassword,
+  subscribeToNews,
+  agreeToTerms,
+}) {
+  const e = getEmployee(_id)
+  if (email !== confirmEmail) {
+    throw new Error('Email and confirm email must match')
+  }
+
+  if (password !== confirmPassword) {
+    throw new Error('Password and confirm password must match')
+  }
+
+  // TODO hash password
+
+  e.email = email
+  e.password = password
+  e.subscribeToNews = subscribeToNews
+  e.agreeToTerms = agreeToTerms
+
+  e.step = 4
+
+  return e.save()
+}
+
 
 function createEmployerLead({
   firstName,
@@ -21,6 +101,7 @@ function createEmployerLead({
   }).save()
 }
 
+
 function createEmployeeLead({
   firstName,
   lastName,
@@ -39,7 +120,12 @@ function createEmployeeLead({
   }).save()
 }
 
+
 module.exports = {
   createEmployerLead,
   createEmployeeLead,
+
+  execEmployeeAppStep1,
+  execEmployeeAppStep2,
+  execEmployeeAppStep3,
 }
---
+++ server/database/models/Employee.js
@@ -0,0 +1,223 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const EmployeeSchema = new Schema({
+  // General
+  step: {
+    type: Number,
+    default: 1,
+    minValue: 1,
+    maxValue: 8,
+  },
+
+
+  // Step 1
+  loanAmount: {
+    type: Number,
+    required: [true, 'You must select a loan amount'],
+  },
+  loanTerm: {
+    type: Number,
+    required: [true, 'You must select a loan term'],
+    min: [3, 'Loan must be at least 3 months'],
+    max: [36, 'Loan is at most 36 months'],
+  },
+
+
+  // Step 2
+  firstName: {
+    type: String,
+    maxLength: 500,
+    minLength: 1,
+  },
+  lastName: {
+    type: String,
+    maxLength: 500,
+    minLength: 1,
+  },
+  suffix: {
+    type: String,
+    maxLength: 20,
+  },
+  dateOfBirth: {
+    day: {
+      minValue: 1,
+      maxValue: 31,
+      type: Number,
+    },
+    month: {
+      minValue: 1,
+      maxValue: 12,
+      type: Number,
+    },
+    year: {
+      minValue: 1850,
+      maxValue: 2050,
+      type: Number,
+    },
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator: v => /\d{3}-\d{3}-\d{4}/.test(v),
+      message: props => `${props.value} is not a valid phone number!`,
+    },
+  },
+
+
+  // Step 3
+  email: {
+    type: String,
+    unique: true,
+    maxLength: 500,
+    minLength: 1,
+    validate: {
+      validator: (v) => {
+        const emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/ // eslint-disable-line
+        return emailRegex.test(v)
+      },
+      message: props => `${props.value} is not a valid email address`,
+    },
+  },
+  password: {
+    type: String,
+    minLength: [2, 'Password too short'],
+  },
+  subscribeToNews: {
+    type: Boolean,
+    default: false,
+  },
+  agreeToTerms: {
+    type: Boolean,
+    validate: {
+      validator: v => Boolean(v), // The value must be truthy
+      message: () => 'You must agree to the terms and conditions to proceed',
+    },
+  },
+
+
+  // Step 4
+  payrollId: {
+    type: String,
+    minLength: 1,
+    maxLength: 1000,
+  },
+  grossAnnualIncome: {
+    type: Number,
+    minValue: [0, 'Income must be non-negative'],
+    maxValue: [999999999, 'Income cannot be in excess of 1 billion'],
+  },
+  otherIncome: {
+    type: Number,
+    minValue: [0, 'Other income must be non-negative'],
+    maxValue: [999999999, 'Other income cannot be in excess of 1 billion'],
+  },
+  employmentStartDate: {
+    month: {
+      type: Number,
+      minValue: 1,
+      maxValue: 12,
+    },
+    year: {
+      type: Number,
+      minValue: 1850,
+      maxValue: 2050,
+    },
+  },
+  paycycle: { // Frequency of pay in weeks
+    type: Number,
+    minValue: 1,
+    maxValue: 52,
+  },
+
+
+  // Step 5
+  address: {
+    address1: {
+      type: String,
+      minLength: 1,
+      maxLength: 500,
+    },
+    address2: {
+      type: String,
+      minLength: 0,
+      maxLength: 500,
+    },
+    city: {
+      type: String,
+      maxLength: 500,
+      minLength: 2,
+    },
+    state: {
+      type: String,
+      minLength: 2,
+      maxLength: 2,
+      validate: {
+        validator: v => ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'].includes(v),
+        message: props => `${props.value} is not a valid state`,
+      },
+    },
+    zip: {
+      type: String,
+      validate: {
+        validator: v => /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(v),
+        message: props => `${props.value} is not a valid zipcode`,
+      },
+    },
+  },
+  timeAtAddress: { // In months
+    type: Number,
+    minValue: 0,
+    maxValue: 2000,
+  },
+  residentialStatus: {
+    type: String,
+    enum: [
+      'Owner with mortgage',
+      'Owner no mortgage',
+      'Renting',
+      'Living with parents',
+      'Government housing',
+      'Other',
+    ],
+  },
+  residentialStatusExplanation: {
+    type: String,
+    minLength: 0,
+    maxLength: 500,
+  },
+
+
+  // Step 6
+  ssn: { // Ref: zparacha.com/validate-social-security-number-using-javascript-regular-expressions
+    type: String,
+    validate: {
+      validator: v => /^[0-9]{3}?[0-9]{2}?[0-9]{4}$/.test(v),
+      message: () => 'Please enter a valid SSN',
+    },
+  },
+  numberOfFinancialDependents: {
+    type: Number,
+    minValue: 0,
+    maxValue: 50,
+  },
+  civilStatus: {
+    type: String,
+    minLength: 1,
+    maxLength: 200,
+  },
+  expectsChangesToEmploymentStatus: {
+    type: Boolean,
+  },
+
+  // Step 7
+  applied: {
+    type: Boolean,
+    default: false,
+  },
+}, {
+  timestamps: true,
+})
+
+module.exports = mongoose.model('Employee', EmployeeSchema)
